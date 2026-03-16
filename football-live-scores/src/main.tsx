2cecd287a2978150674ae9d4f1b25a528658efe3
alejocastaram <alejo.castaram@gmail.com>
2026-03-16 15:44:34 -0500

    fix routing

--- football-live-scores/src/main.tsx
+++ football-live-scores/src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import App from './App.tsx'
+import { Home } from './features/home/Home'
+import { CreateFootballMatchForm } from './features/create-football-match/CreateFootballMatchForm'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-football-match" element={<CreateFootballMatchForm />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
